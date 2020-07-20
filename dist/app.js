850343cc5d5939b7ac2e0ef40ea6213bb1cacaba
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-07-20 11:08:39 +0300

    added dummy error handling

--- dist/app.js
+++ dist/app.js
@@ -5,8 +5,25 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
+const http_errors_1 = __importDefault(require("./models/http-errors"));
 const places_routes_1 = __importDefault(require("./routes/places-routes"));
 const app = express_1.default();
 app.use(body_parser_1.default.json());
 app.use('/places', places_routes_1.default);
+app.use((req, res, next) => {
+    const error = new http_errors_1.default('Couldnt find this route', 404);
+    return res.json({ message: error.message });
+});
+app.use((error, req, res, next) => {
+    const status = error.code || 500;
+    const message = error.message || 'Something went wrong';
+    if (!res.headersSent) {
+        return next(error);
+    }
+    res
+        .status(status)
+        .json({
+        message: message,
+    });
+});
 app.listen({ port: 8000 });
--- src/app.ts
+++ src/app.ts
@@ -11,5 +11,24 @@ app.use(bodyParser.json());
 app.use('/places', placesRouter);
 
 
+app.use((req: Request, res: Response, next: NextFunction)=>{
+    const error = new HttpError('Couldnt find this route', 404);
+    return res.json({message:error.message});
+})
+
+app.use((error: HttpError, req: Request, res: Response, next: NextFunction) => {
+    const status = error.code || 500;
+    const message = error.message || 'Something went wrong';
+
+    if(!res.headersSent){
+        return next(error);
+    }
+
+    res
+        .status(status)
+        .json({
+            message: message,
+        })
+})
 
 app.listen({ port: 8000 });

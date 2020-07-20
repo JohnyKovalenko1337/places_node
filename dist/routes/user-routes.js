c78a3247756c42e8ec5c8e59c896d8033aab055e
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-07-20 12:16:44 +0300

    added dummy validation for signup

--- dist/routes/user-routes.js
+++ dist/routes/user-routes.js
@@ -5,6 +5,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const user_controller_1 = require("../controllers/user-controller");
+const express_validator_1 = require("express-validator");
 const router = express_1.default.Router();
 router.get('/', user_controller_1.getUserById);
+router.post('/signup', [
+    express_validator_1.check('name')
+        .notEmpty()
+        .isLength({ min: 3 }),
+    express_validator_1.check('email')
+        .isEmail(),
+    express_validator_1.check('password')
+        .isLength({ min: 6 })
+], user_controller_1.signUp);
+router.post('/login', user_controller_1.login);
 exports.default = router;
--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -1,9 +1,23 @@
 import express from 'express';
-
-import { getUserById } from '../controllers/user-controller';
+import { getUserById, signUp, login } from '../controllers/user-controller';
+import { check } from 'express-validator';
 
 const router = express.Router();
 
 router.get('/', getUserById);
+
+router.post('/signup',
+    [
+        check('name')
+        .notEmpty()
+        .isLength({min:3}),
+        check('email')
+        .isEmail(),
+        check('password')
+        .isLength({min:6})
+    ],
+    signUp);
+
+router.post('/login', login);
 
 export default router;

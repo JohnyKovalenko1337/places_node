67ae145bf11babace3bc10ce693aba4d9b947e0c
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-07-20 12:17:43 +0300

    added error handling for signup

--- dist/controllers/user-controller.js
+++ dist/controllers/user-controller.js
@@ -1,7 +1,21 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getUserById = void 0;
+exports.login = exports.signUp = exports.getUserById = void 0;
+const express_validator_1 = require("express-validator");
+const http_errors_1 = __importDefault(require("../models/http-errors"));
 exports.getUserById = (req, res, next) => {
     const userId = req.params.userId;
     res.json({ message: 'success' });
+};
+exports.signUp = (req, res, next) => {
+    const errors = express_validator_1.validationResult(req);
+    if (!errors.isEmpty()) {
+        res.status(422);
+        throw new http_errors_1.default('invalid inputs passed', 422);
+    }
+};
+exports.login = (req, res, next) => {
 };
--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
+import { validationResult } from 'express-validator';
 import HttpError from '../models/http-errors';
 
 export const getUserById = (req: Request, res: Response, next: NextFunction) => {
@@ -6,4 +7,17 @@ export const getUserById = (req: Request, res: Response, next: NextFunction) => 
     const userId: string = req.params.userId;
 
     res.json({ message: 'success' })
+};
+
+export const signUp = (req: Request, res: Response, next: NextFunction) => {
+    const errors: any = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        res.status(422)
+        throw new HttpError('invalid inputs passed', 422);
+    }
+};
+
+export const login = (req: Request, res: Response, next: NextFunction) => {
+
 };

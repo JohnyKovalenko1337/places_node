ae9e416861dd4b780cd9316374487d53f991016d
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-07-20 21:26:28 +0300

    added placeSchema for mongoose

---
+++ dist/models/placeSchema.js
@@ -0,0 +1,34 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importDefault(require("mongoose"));
+const Schema = mongoose_1.default.Schema;
+const placeSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    imageUrl: {
+        type: String,
+        required: true
+    },
+    location: {
+        lat: { type: Number, required: true },
+        lng: { type: Number, required: true },
+    },
+    creator: {
+        type: String,
+        required: true
+    },
+});
+exports.default = mongoose_1.default.model('Place', placeSchema);

441af17e91afe7431a72736eac3209b759b5d472
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-07-21 12:19:32 +0300

    added mongoose logic for user controller

--- dist/controllers/user-controller.js
+++ dist/controllers/user-controller.js
@@ -1,21 +1,78 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.login = exports.signUp = exports.getUserById = void 0;
+exports.login = exports.signUp = exports.getUsers = void 0;
 const express_validator_1 = require("express-validator");
 const http_errors_1 = __importDefault(require("../models/http-errors"));
-exports.getUserById = (req, res, next) => {
-    const userId = req.params.userId;
-    res.json({ message: 'success' });
-};
-exports.signUp = (req, res, next) => {
+const userSchema_1 = __importDefault(require("../models/userSchema"));
+exports.getUsers = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    //onst userId: string = req.params.userId;
+    let users;
+    try {
+        users = yield userSchema_1.default.find({}, 'email name');
+    }
+    catch (err) {
+        next(new http_errors_1.default('Failed operation', 500));
+    }
+    res.json({ message: 'success', users: users.toObject() });
+});
+exports.signUp = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     const errors = express_validator_1.validationResult(req);
     if (!errors.isEmpty()) {
-        res.status(422);
-        throw new http_errors_1.default('invalid inputs passed', 422);
+        next(new http_errors_1.default('invalid inputs passed', 422));
     }
-};
-exports.login = (req, res, next) => {
-};
+    const { name, email, password } = req.body;
+    let existingUser;
+    try {
+        existingUser = yield userSchema_1.default.findOne({ email: email });
+    }
+    catch (err) {
+        next(new http_errors_1.default('Failed operation', 500));
+    }
+    if (existingUser) {
+        return next(new http_errors_1.default('User with this email already exists', 422));
+    }
+    const createUser = new userSchema_1.default({
+        name,
+        email,
+        imageUrl: 'https://i.pinimg.com/originals/92/c2/f0/92c2f03407ee7bc8dab7c2962388a139.jpg',
+        password,
+        places: []
+    });
+    try {
+        yield createUser.save();
+    }
+    catch (error) {
+        next(new http_errors_1.default('cant save new place', 500));
+    }
+    res.status(201).json({ message: "place successfuly added", user: createUser.toObject() });
+});
+exports.login = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    const errors = express_validator_1.validationResult(req);
+    if (!errors.isEmpty()) {
+        next(new http_errors_1.default('invalid inputs passed', 422));
+    }
+    const { email, password } = req.body;
+    let existingUser;
+    try {
+        existingUser = yield userSchema_1.default.findOne({ email: email, password: password });
+    }
+    catch (err) {
+        next(new http_errors_1.default('Failed operation', 500));
+    }
+    if (!existingUser) {
+        next(new http_errors_1.default('Invalid email or password', 422));
+    }
+    res.status(201).json({ message: "User successfuly loggined" });
+});
--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -1,23 +1,79 @@
 import { Request, Response, NextFunction } from 'express';
 import { validationResult } from 'express-validator';
 import HttpError from '../models/http-errors';
+import User from '../models/userSchema';
 
-export const getUserById = (req: Request, res: Response, next: NextFunction) => {
+export const getUsers = async (req: Request, res: Response, next: NextFunction) => {
 
-    const userId: string = req.params.userId;
+    //onst userId: string = req.params.userId;
+    let users: any;
+    try {
+        users = await User.find({}, 'email name');
+    }
+    catch (err) {
+        next(new HttpError('Failed operation', 500));
+    }
 
-    res.json({ message: 'success' })
+    res.json({ message: 'success', users: users.toObject() })
 };
 
-export const signUp = (req: Request, res: Response, next: NextFunction) => {
+export const signUp = async (req: Request, res: Response, next: NextFunction) => {
     const errors: any = validationResult(req);
 
     if (!errors.isEmpty()) {
-        res.status(422)
-        throw new HttpError('invalid inputs passed', 422);
+        next(new HttpError('invalid inputs passed', 422));
     }
+    const { name, email, password } = req.body;
+
+    let existingUser: any;
+    try {
+        existingUser = await User.findOne({ email: email });
+    }
+    catch (err) {
+        next(new HttpError('Failed operation', 500));
+    }
+    if (existingUser) {
+        return next(new HttpError('User with this email already exists', 422));
+    }
+    const createUser: any = new User({
+        name,
+        email,
+        imageUrl: 'https://i.pinimg.com/originals/92/c2/f0/92c2f03407ee7bc8dab7c2962388a139.jpg',
+        password,
+        places: []
+    });
+    try {
+        await createUser.save()
+    }
+    catch (error) {
+        next(new HttpError('cant save new place', 500));
+    }
+    res.status(201).json({ message: "place successfuly added", user: createUser.toObject() });
+
 };
 
-export const login = (req: Request, res: Response, next: NextFunction) => {
+export const login = async (req: Request, res: Response, next: NextFunction) => {
+
+    const errors: any = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        next(new HttpError('invalid inputs passed', 422));
+    }
+
+    const { email, password } = req.body;
+
+    let existingUser: any;
+    try {
+        existingUser = await User.findOne({ email: email, password: password });
+    }
+    catch (err) {
+        next(new HttpError('Failed operation', 500));
+    }
+
+    if (!existingUser) {
+        next(new HttpError('Invalid email or password', 422));
+    }
+
+    res.status(201).json({ message: "User successfuly loggined" });
 
 };

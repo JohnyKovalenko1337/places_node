246e8f8f8025e49df93b377b1cbe7bf301dc96d4
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-07-21 12:15:10 +0300

    added user Router

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 
 import HttpError from './models/http-errors';
 import placesRouter from './routes/places-routes';
+import UserRouter from './routes/user-routes';
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(bodyParser.json());
 
 app.use('/places', placesRouter);
 
+app.use('/user', UserRouter);
 
 app.use((req: Request, res: Response, next: NextFunction) => {
     const error = new HttpError('Couldnt find this route', 404);
@@ -21,11 +23,7 @@ app.use((error: HttpError, req: Request, res: Response, next: NextFunction) => {
     const status = error.code || 500;
     const message = error.message || 'Something went wrong';
 
-    if (!res.headersSent) {
-        return next(error);
-    }
-
-    res
+    return res
         .status(status)
         .json({
             message: message,

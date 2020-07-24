8d67b35deb3650d308442cd43666ee86d9a5d35f
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-07-24 19:19:09 +0300

    added image upload to create rout

--- dist/routes/places-routes.js
+++ dist/routes/places-routes.js
@@ -5,11 +5,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const express_validator_1 = require("express-validator");
+const file_upload_1 = require("../middleware/file-upload");
 const places_controller_1 = require("../controllers/places-controller");
 const router = express_1.default.Router();
 router.get('/:placeId', places_controller_1.getPlaceById);
 router.get('/user/:userId', places_controller_1.getPlacesByUserId);
-router.post('/create', express_validator_1.check('title').notEmpty(), express_validator_1.check('description').isLength({ min: 5 }), express_validator_1.check('address').notEmpty(), places_controller_1.createPlace);
+router.post('/create', file_upload_1.fileUpload.single('image'), express_validator_1.check('title').notEmpty(), express_validator_1.check('description').isLength({ min: 5 }), express_validator_1.check('address').notEmpty(), places_controller_1.createPlace);
 router.patch('/:placeId', express_validator_1.check('title').notEmpty(), express_validator_1.check('description').isLength({ min: 5 }), places_controller_1.updatePlaceById);
 router.delete('/:placeId', places_controller_1.deletePlaceById);
 exports.default = router;
--- src/routes/places-routes.ts
+++ src/routes/places-routes.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { check } from 'express-validator';
-
+import { fileUpload } from '../middleware/file-upload';
 import { getPlaceById, createPlace, getPlacesByUserId, updatePlaceById, deletePlaceById } from '../controllers/places-controller';
 
 const router = express.Router();
@@ -10,6 +10,7 @@ router.get('/:placeId', getPlaceById);
 router.get('/user/:userId', getPlacesByUserId);
 
 router.post('/create',
+    fileUpload.single('image'),
     check('title').notEmpty(),
     check('description').isLength({ min: 5 }),
     check('address').notEmpty(),

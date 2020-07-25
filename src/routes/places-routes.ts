22a144b04706f9729e979e08191bd72a1fb5c074
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-07-25 14:15:32 +0300

    added auth middleware before some routes

--- src/routes/places-routes.ts
+++ src/routes/places-routes.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { check } from 'express-validator';
 import { fileUpload } from '../middleware/file-upload';
+import { auth } from '../middleware/auth';
 import { getPlaceById, createPlace, getPlacesByUserId, updatePlaceById, deletePlaceById } from '../controllers/places-controller';
 
 const router = express.Router();
@@ -8,6 +9,8 @@ const router = express.Router();
 router.get('/:placeId', getPlaceById);
 
 router.get('/user/:userId', getPlacesByUserId);
+
+router.use(auth);
 
 router.post('/create',
     fileUpload.single('image'),

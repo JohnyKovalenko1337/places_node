49ad150d0c7309516e93249267b242998e1fbc89
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-07-27 23:48:39 +0300

    added aws s3 logic for file upload

--- dist/middleware/file-upload.js
+++ dist/middleware/file-upload.js
@@ -5,28 +5,30 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.fileUpload = void 0;
 const multer_1 = __importDefault(require("multer"));
-const uuid_1 = require("uuid");
+const path_1 = __importDefault(require("path"));
+const aws_sdk_1 = __importDefault(require("aws-sdk"));
+const multer_s3_1 = __importDefault(require("multer-s3"));
+const s3 = new aws_sdk_1.default.S3({
+    accessKeyId: 'AKIAZEV2VUWLW3BXFOU5',
+    secretAccessKey: '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',
+});
 const Mime_Type_Map = {
     'image/png': 'png',
     'image/jpg': 'jpg',
     'image/jpeg': 'jpeg'
 };
 exports.fileUpload = multer_1.default({
-    limits: {
-        fileSize: 500000
-    },
-    storage: multer_1.default.diskStorage({
-        destination: (req, file, cb) => {
-            cb(null, './images');
-        },
-        filename: (req, file, cb) => {
-            const ext = Mime_Type_Map[file.mimetype];
-            cb(null, uuid_1.v4());
-        },
+    storage: multer_s3_1.default({
+        s3: s3,
+        bucket: 'image-storing-bucket',
+        key: function (req, file, cb) {
+            cb(null, path_1.default.basename(file.originalname, path_1.default.extname(file.originalname)) + '-' + Date.now() + path_1.default.extname(file.originalname));
+        }
     }),
+    limits: { fileSize: 2000000 },
     fileFilter: (req, file, cb) => {
         const isValid = !!Mime_Type_Map[file.mimetype];
         let err = isValid ? null : new Error('Invalid mimetype');
         cb(err, isValid);
-    },
+    }
 });
--- src/middleware/file-upload.ts
+++ src/middleware/file-upload.ts
@@ -1,31 +1,37 @@
 import { Request } from 'express';
 import multer, { diskStorage } from 'multer';
-import { v4 } from 'uuid'
+import path from 'path';
+import aws from 'aws-sdk';
+import multerS3 from 'multer-s3';
 
-const Mime_Type_Map:  any = {
-    'image/png':'png',
-    'image/jpg':'jpg',
-    'image/jpeg':'jpeg'
+const s3: AWS.S3 = new aws.S3({
+    accessKeyId: 'AKIAZEV2VUWLW3BXFOU5',
+    secretAccessKey: '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',
+    
+});
+
+
+const Mime_Type_Map: any = {
+    'image/png': 'png',
+    'image/jpg': 'jpg',
+    'image/jpeg': 'jpeg'
 }
 
 export const fileUpload = multer({
-    limits:{
-        fileSize: 500000
-    },
-    storage: multer.diskStorage({
-        destination: (req: Request, file: any, cb)=>{
-            cb(null, './images')
-        },
-        filename: (req: Request, file: any, cb)=>{
-            const ext: any = Mime_Type_Map[file.mimetype];
-            cb(null, v4())
-        },
-        
+    storage: multerS3({
+        s3: s3,
+        bucket: 'image-storing-bucket',
+        key: function (req, file, cb) {
+            cb(null, path.basename(file.originalname, path.extname(file.originalname)) + '-' + Date.now() + path.extname(file.originalname))
+        }
     }),
-    fileFilter: (req: Request, file: any, cb)=>{
+    limits: { fileSize: 2000000 },
+    fileFilter: (req: Request, file: any, cb) => {
         const isValid: boolean | undefined = !!Mime_Type_Map[file.mimetype];
-        let err: any = isValid ? null: new Error('Invalid mimetype');
+        let err: any = isValid ? null : new Error('Invalid mimetype');
         cb(err, isValid)
-    },
-
+    }
 });
+
+
+
